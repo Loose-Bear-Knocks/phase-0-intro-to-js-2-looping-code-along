6743de1338950c4e96677718089fcb136eb9e735
leif enoksen <enoksen.enoksen@gmail.com>
2023-04-27 20:45:18 -0400

    PW Complete Loop Code Along

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const names = [];
+function writeCards(names) {
+  let cards = []
+  for (let i = 0; i < names.length; i++) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return cards;
+}
+writeCards(names);
+
+debugger;
+
+function countDown(i){
+  for (let i = 0; i < 11 ; i++)
+      console.log (i);
+  }
+  function VarRedeclaration(i) {
+    for(let i = 4; i > 0; i--)
+      console.log (i);
+  }
+  
